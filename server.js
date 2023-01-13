edac29dac9d5e11b8e0713c959c138fbf9758ef0
Balint Korosi <balint.j.korosi@gmail.com>
2023-01-13 16:55:12 +0100

    routes

--- server.js
+++ server.js
@@ -1,3 +1,42 @@
 const express = require('express');
 const app = express();
+
+const database = {
+  users: [
+    {
+      id: '123',
+      name: 'John',
+      email: 'john@gmail.com',
+      password: 'cookies',
+      entries: 0,
+      joined: new Date(),
+    },
+    {
+      id: '124',
+      name: 'Sally',
+      email: 'sally@gmail.com',
+      password: 'bananas',
+      entries: 0,
+      joined: new Date(),
+  ],
+};
+
+app.get('/', (req, res) => {
+  res.send('Hello World!');
+});
+
+// /signin --> POST = success/fail
+app.post('/signin', (req, res) => {
+  res.json('signing');
+});
+
+
 app.listen(3000, () => console.log('Server is running on port 3000'));
+
+/*
+/--> res = this is working
+
+/register --> POST = user
+/profile/:userId --> GET = user
+/image --> PUT (updating the number of pics a user submitted so that we know the ranking) --> user
+*/
