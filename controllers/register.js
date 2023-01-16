e45cba11a251c00be10c51ebaeba5d7f57665733
Balint Korosi <balint.j.korosi@gmail.com>
2023-01-16 15:14:09 +0100

    final commit  before moving clarify to backend

--- controllers/register.js
+++ controllers/register.js
@@ -1,6 +1,9 @@
 
 const handleRegsiter = (req, res, db, bcrypt) => {
   const { email, name, password } = req.body;
+  if (!email || !name || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
   console.log('body', req.body)
   console.log('password', password)
   const hash = bcrypt.hashSync(password);
--- controllers/signin.js
+++ controllers/signin.js
@@ -5,14 +5,18 @@
   // } else {
   //   res.status(400).json('error logging in');
   // }
+  const { email, password } = req.body;
+  if(!email || !password) {
+    return res.status(400).json('incorrect form submission');
+  }
 
   db.select('email', 'hash').from('login')
-  .where('email', '=', req.body.email)
+  .where('email', '=', email)
   .then(data => {
-    const isValid = bcrypt.compareSync(req.body.password, data[0].hash)
+    const isValid = bcrypt.compareSync(password, data[0].hash)
     console.log(isValid)
     if(isValid) {
-      return db.select('*').from('users').where('email', '=', req.body.email)
+      return db.select('*').from('users').where('email', '=', email)
       .then(user => {
         res.json(user[0])
       })
