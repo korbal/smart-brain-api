acb98ef3c98d658e77ca19d1f530ba37f07a36bc
Balint Korosi <balint.j.korosi@gmail.com>
2023-01-17 07:27:23 +0100

    e

--- server.js
+++ server.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const app = express();
-app.use(express.json());
 const bcrypt = require('bcrypt-nodejs');
 const cors = require('cors');
 const knex = require('knex');
@@ -8,6 +7,12 @@ const register = require('./controllers/register');
 const signin = require('./controllers/signin');
 const profile = require('./controllers/profile');
 const image = require('./controllers/image');
+const POSTGRES_PASSWORD = process.env.POSTGRES_PASSWORD;
+
+console.log(process.env.CLARIFY_PAT);
+console.log(process.env.POSTGRES_PASSWORD);
+
+app.use(express.json());
 
 const db  = knex({
   client: 'pg',
@@ -79,4 +84,5 @@ app.put('/image', (req, res) => { image.handleImage(req, res, db) });
 app.post('/imageurl', (req, res) => { image.handleApiCall(req, res) });
 
 // PORT
-app.listen(3000, () => console.log('Server is running on port 3000'));
+
+app.listen(3000, () => console.log(`Server is running on port 3000`));
